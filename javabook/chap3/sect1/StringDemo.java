5b23ea9a9bf223ff7e1fbcf6f4168210bb2a3db0
dcoles-javabook <243444915+dcoles-javabook@users.noreply.github.com>
2025-11-11 19:17:20 -0500

    Create StringDemo.java

---
+++ javabook/chap3/sect1/StringDemo.java
@@ -0,0 +1,23 @@
+package javabook.chap3.sect1;
+
+/**
+ * Demonstrates string concatenation.
+ *
+ * @author Drue Coles
+ */
+public class StringDemo {
+
+    public static void main(String[] args) {
+        String s1 = "butter";
+        String s2 = "fly";
+        String s3 = s1 + s2;
+        System.out.println(s3);
+
+        System.out.println(2 + 3 + 5);
+        System.out.println("2 + 3 + 5");
+        System.out.println("2" + "3" + "5");
+        System.out.println("2" + 3 + "5");
+        System.out.println("2" + 3 + 5);
+        System.out.println(2 + 3 + "5");
+    }
+}
