e7b2081814f8123db4db5166f790470d258a50cf
dcoles-javabook <243444915+dcoles-javabook@users.noreply.github.com>
2025-11-15 09:35:24 -0500

    Create StringDemo3.java

---
+++ javabook/chap3/sect1/StringDemo3.java
@@ -0,0 +1,22 @@
+package javabook.chap3.sect1;
+
+/**
+ * Demonstrates string immutability.
+ *
+ * @author Drue Coles
+ */
+public class StringDemo3 {
+
+   public static void main(String[] args) {
+      String animal = "quokka";
+      System.out.println(animal);
+
+      // toUpperCase does not mutate the string
+      System.out.println(animal.toUpperCase());
+      System.out.println(animal);
+
+      // changing the reference, not the referent
+      animal = animal.toUpperCase();
+      System.out.println(animal);
+   }
+}
