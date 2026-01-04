37e1fdc2fe955a9c589df0a717eb912514332c79
dcoles-javabook <243444915+dcoles-javabook@users.noreply.github.com>
2026-01-03 22:24:13 -0500

    Update ThorVsZeus.java

--- javabook/chap8/sect5/ThorVsZeus.java
+++ javabook/chap8/sect5/ThorVsZeus.java
@@ -1,39 +1,49 @@
 package javabook.chap8.sect5;
 
-import java.util.Arrays;
-
 /**
- * Calculates the probability of rolling a full house. Five numbers form a full house if two are
- * equal and the remaining three are equal.
+ * Calculates the probability that Thor beats Zeus. Thor has nine 4-sided dice and Zeus has six
+ * 6-sided dice. They roll their dice and add up the numbers. The highest total wins.
  *
  * @author Drue Coles
  */
-public class FullHouse {
+public class ThorVsZeus {
 
    public static void main(String[] args) {
-      CupOfDice cup = new CupOfDice(5);
+      CupOfDice thorCup = new CupOfDice(9, 4); // nine 4-sided dice
+      CupOfDice zeusCup = new CupOfDice(6); // six 6-sided dice
 
       // Monte Carlo simulation
-      final int trials = 10_000_000;
-      int wins = 0;
-      for (int i = 0; i < trials; i++) {
-         cup.roll();
-         if (isFullHouse(cup.getDice())) {
-            wins++;
+      final int games = 10_000_000;
+      int thorWins = 0;
+      for (int i = 0; i < games; i++) {
+         if (play(thorCup, zeusCup)) {
+            thorWins++;
          }
       }
 
-      double prob = (double) wins / trials * 100;
-      System.out.printf("Full house probability: %.2f%% %n", prob);
+      double prob = (double) thorWins / games * 100;
+      System.out.printf("Thor beats Zeus with probability %.2f%% %n", prob);
+   }
+   
+   /**
+    * Rolls both cups and returns true if Thor's total is higher than Zeus's.
+    */
+   private static boolean play(CupOfDice thorCup, CupOfDice zeusCup) {
+      thorCup.roll();
+      zeusCup.roll();
+      int s1 = sum(thorCup.getDice());
+      int s2 = sum(zeusCup.getDice());
+      return s1 > s2;
    }
 
    /**
-    * Returns true if a given array contains a full house.
+    * Returns the sum of values in an array.
     */
-   private static boolean isFullHouse(int[] values ) {
-      Arrays.sort(values);
-      boolean twoThenThree = values[0] == values[1] && values[2] == values[4];
-      boolean threeThenTwo = values[0] == values[2] && values[3] == values[4];
-      return twoThenThree || threeThenTwo;
+   private static int sum(int[] values) {
+      int total = 0;
+      for (int val : values) {
+         total += val;
+      }
+      return total;
    }
 }
