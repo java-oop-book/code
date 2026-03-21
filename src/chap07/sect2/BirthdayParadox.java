559d1237485ff315bb9dbbc296c8014545222e04
drue.coles <drue-coles@users.noreply.github.com>
2026-03-21 05:03:27 -0400

    Add programs for Section 7.2

---
+++ src/chap07/sect2/BirthdayParadox.java
@@ -0,0 +1,48 @@
+package chap07.sect2;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Estimates the probability that at least two people in a random sample share the same birthday.
+ *
+ * @author Drue Coles
+ */
+public class BirthdayParadox {
+
+   public static void main(String[] args) {
+      System.out.print("Number of birthdays to sample: ");
+      Scanner in = new Scanner(System.in);
+      final int sampleSize = in.nextInt();
+      final int trials = 10_000_000;
+      int matches = 0;
+
+      // Monte Carlo simulation
+      System.out.printf("Simulating %,d trials... %n", trials);
+      for (int i = 0; i < trials; i++) {
+         if (sampleHasMatch(sampleSize)) {
+            matches++;
+         }
+      }
+
+      double prob = (double) matches / trials * 100;
+      System.out.printf("Probability of a birthday match: %.2f%% %n", prob);
+   }
+
+   /**
+    * Samples random birthdays and returns true if any day is chosen twice.
+    */
+   private static boolean sampleHasMatch(int sampleSize) {
+      final int daysInYear = 365;
+      boolean[] previouslyChosen = new boolean[daysInYear];
+
+      for (int i = 0; i < sampleSize; i++) {
+         int j = ThreadLocalRandom.current().nextInt(daysInYear); // choose random birthday
+         if (previouslyChosen[j]) {
+            return true;
+         }
+         previouslyChosen[j] = true;
+      }
+      return false;
+   }
+}
---
+++ src/chap07/sect2/RandomizedPlaylist.java
@@ -0,0 +1,53 @@
+package chap07.sect2;
+
+import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * Prompts the user for the number of songs in a randomized playlist and outputs the expected number
+ * of songs that will be played until every song is heard at least once.
+ *
+ * @author Drue Coles
+ */
+public class RandomizedPlaylist {
+
+   public static void main(String[] args) {
+      System.out.print("Number of songs in the playlist: ");
+      Scanner in = new Scanner(System.in);
+      int n = in.nextInt();
+
+      final int trials = 1_000_000;
+      int total = 0;  // songs played in all trials
+
+      // Monte Carlo simulation
+      System.out.printf("Simulating %,d trials... %n", trials);
+      for (int i = 0; i < trials; i++) {
+         total += playSongs(n);
+      }
+
+      double result = (double) total / trials;
+      System.out.printf("Expected number of songs until all are played: %.2f %n", result);
+   }
+
+   /**
+    * Samples songs at random from a playlist until every song is heard at least once.
+    *
+    * @param n number of songs in the playlist
+    * @return total number of songs played
+    */
+   private static int playSongs(int n) {
+      boolean[] alreadyPlayed = new boolean[n];
+      int totalSongsPlayed = 0;
+      int distinctSongsPlayed = 0;
+
+      while (distinctSongsPlayed < n) {
+         int songIndex = ThreadLocalRandom.current().nextInt(n);
+         totalSongsPlayed++;
+         if (!alreadyPlayed[songIndex]) {
+            distinctSongsPlayed++;
+         }
+         alreadyPlayed[songIndex] = true;
+      }
+      return totalSongsPlayed;
+   }
+}
